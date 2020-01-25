96a8249cc32d7157209a28bb6d0cc43e8fec5e79
Aviva Weinbaum <aw3156@barnard.edu>
2020-01-24 21:54:34 -0500

    small changes

--- sketch.js
+++ sketch.js
@@ -15,14 +15,12 @@ function draw() {
 
 	let radius_hour = map(hour(), 0, 24, 0, 800, true)/2;
 	let radius_minute = map(minute(), 0, 60, 0, radius_hour, true)/2;
-	let radius_sec = map(second(), 0, 60, 0, radius_minute, true)/2;
+	let radius_sec = map(second(), 0, 60, 0, radius_minute)/2;
+	console.log(radius_sec);
 	//let hx = map(mouseX,0,width,350,450,true);
 	//let hy = map(mouseY,0,height,200,400,true);
 	let mx = map(mouseX,0,width, 350, 450, true);
 	let my = map(mouseY,0,height, 200, 400, true);
-	let sx = mx;
-	let sy = my;
-	console.log()
 
 	noFill();
 	colorMode(RGB,255,255,255,1);
@@ -32,9 +30,10 @@ function draw() {
 	ellipse(400, 300, radius_hour, radius_hour);
 
 	stroke(0,0,255,0.4);
-	ellipse(mx, my, radius_minute, radius_minute);
+	circle(mx, my, radius_minute);
 
 	stroke(255,255,0,0.4);
-	ellipse(sx, sy, radius_sec, radius_sec);
+	circle(mx, my, radius_sec);
+	//ellipse(sx, sy, radius_sec, radius_sec);
 
 }
